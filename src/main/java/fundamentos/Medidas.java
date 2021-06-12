86d0052f777a0acd01bd6c2e9a114f80f87c1a1d
renanbortoletto <85020822+renanbortoletto@users.noreply.github.com>
2021-06-12 12:03:51 -0300

    Fundamentos do Java 12 de Junho de 2021

---
+++ src/main/java/fundamentos/Medidas.java
@@ -0,0 +1,27 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class Medidas {
+
+
+
+    public static void main(String[] args){
+        System.out.println("Escolha o Calculo Desejado");
+        System.out.println("(1) Area do Quadrado");
+        System.out.println("(2) Area do Retangulo");
+        System.out.println("(3) Area do Triangulo");
+        System.out.println("(4) Area do Circulo");
+
+    }
+
+    public static int calcularAreaDoQuadrado(){
+        Scanner entrada = new Scanner(System.in);  //Criamos o objeto para ter ler uma entrada no console
+        System.out.println("Digite o tamanho do lado: ");
+        
+
+    }
+
+
+
+}
